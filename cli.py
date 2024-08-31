951365404329a10ae16f8ecfd0f2288769ee3c70
Lucas Valdez <lnvaldez24@gmail.com>
2024-08-31 14:13:00 -0400

    Create location command

--- cli.py
+++ cli.py
@@ -1,6 +1,28 @@
 import typer
+from weather_models import WeatherLocation
 
 app = typer.Typer()
+
+
+@app.command()
+def location(
+    city: str,
+    current: bool = False,
+    forecast: bool = False,
+    aqi: bool = False,
+    sun: bool = False,
+):
+    weather = WeatherLocation(city_name=city)
+
+    if current:
+        print(weather.current_data)
+    if forecast:
+        print(weather.forecast)
+    if aqi:
+        print(weather.air_quality)
+    if sun:
+        print(weather.sun_data)
+
 
 if __name__ == "__main__":
     app()
