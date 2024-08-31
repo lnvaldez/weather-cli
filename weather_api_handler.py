457be59c3f8222a1560a6ac9ea79091bcb523b6c
Lucas Valdez <lnvaldez24@gmail.com>
2024-08-31 14:19:26 -0400

    Add @classmethod decorator to WeatherApi methods

--- weather_api_handler.py
+++ weather_api_handler.py
@@ -21,18 +21,22 @@ class WeatherApi:
         response = requests.get(url)
         return response.json()
 
+    @classmethod
     def get_weather_by_city(self, city: str) -> Dict[str, Any]:
         url = f"{self.BASE_URLS['weather']}appid={self.WEATHER_API_KEY}&q={city}&units=metric"
         return self.fetch_data(url=url)
 
+    @classmethod
     def get_weather_by_gcs(self, lat: float, lon: float) -> Dict[str, Any]:
         url = f"{self.BASE_URLS['weather']}appid={self.WEATHER_API_KEY}&lat={lat}&lon={lon}&units=metric"
         return self.fetch_data(url=url)
 
+    @classmethod
     def get_forecast(self, lat: float, lon: float) -> Dict[str, Any]:
         url = f"{self.BASE_URLS['forecast']}appid={self.WEATHER_API_KEY}&lat={lat}&lon={lon}&units=metric"
         return self.fetch_data(url=url)
 
+    @classmethod
     def get_air_quality(self, lat: float, lon: float) -> Dict[str, Any]:
         url = f"{self.BASE_URLS['air']}appid={self.WEATHER_API_KEY}&lat={lat}&lon={lon}&units=metric"
         return self.fetch_data(url=url)
