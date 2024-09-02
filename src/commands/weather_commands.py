e58f50dbad5a6c02e37f8d7babd212ad7510b524
Lucas Valdez <lnvaldez24@gmail.com>
2024-09-02 11:20:22 -0400

    Transfer location and coordinate functions to dedicated weather_commands.py file

---
+++ src/commands/weather_commands.py
@@ -0,0 +1,44 @@
+from src.models.weather_models import WeatherLocation, Coordinate
+from src.utils.enum import OutputFormat
+from src.utils.output_format import output_format
+
+
+def location(
+    city: str,
+    current: bool = False,
+    forecast: bool = False,
+    aqi: bool = False,
+    sun: bool = False,
+    format: OutputFormat = OutputFormat.json,
+):
+    weather = WeatherLocation(city_name=city)
+
+    if current:
+        print(output_format(data=weather.current_data, format_type=format))
+    if forecast:
+        print(output_format(data=weather.forecast, format_type=format))
+    if aqi:
+        print(output_format(data=weather.air_quality, format_type=format))
+    if sun:
+        print(output_format(data=weather.sun_data, format_type=format))
+
+
+def coordinate(
+    latitude: float,
+    longitude: float,
+    current: bool = False,
+    forecast: bool = False,
+    aqi: bool = False,
+    sun: bool = False,
+    format: OutputFormat = OutputFormat.json,
+):
+    weather = Coordinate(lat=latitude, lon=longitude)
+
+    if current:
+        print(output_format(data=weather.current_data, format_type=format))
+    if forecast:
+        print(output_format(data=weather.forecast, format_type=format))
+    if aqi:
+        print(output_format(data=weather.air_quality, format_type=format))
+    if sun:
+        print(output_format(data=weather.sun_data, format_type=format))
