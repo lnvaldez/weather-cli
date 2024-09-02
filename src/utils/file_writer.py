e960c23f84c66262fa14fae00fa51badb0077a76
Lucas Valdez <lnvaldez24@gmail.com>
2024-09-02 12:04:34 -0400

    Fix data path for files

--- src/utils/file_writer.py
+++ src/utils/file_writer.py
@@ -3,9 +3,9 @@ from typing import Union, Dict, List, Any
 from src.utils.format import format_as_json, format_as_csv, format_as_txt
 
 
-json_file = os.path.join("data", "data.json")
-csv_file = os.path.join("data", "data.csv")
-txt_file = os.path.join("data", "data.txt")
+json_file = os.path.join("src/data", "data.json")
+csv_file = os.path.join("src/data", "data.csv")
+txt_file = os.path.join("src/data", "data.txt")
 
 
 def write_to_json_file(data: Union[Dict[str, Any], List[Any]]):
