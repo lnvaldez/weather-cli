b65bfcd70113d5d9de30a0eceb6d14cb5a45cc8d
Lucas Valdez <lnvaldez24@gmail.com>
2024-09-02 11:55:07 -0400

    Include file-writing capabilities to output_format function

--- src/utils/output_format.py
+++ src/utils/output_format.py
@@ -1,57 +1,26 @@
-import csv
-import io
-import json
-from typing import Dict, List, Union, Any
+from typing import Dict, Any
+from src.utils.format import format_as_json, format_as_csv, format_as_txt
+from src.utils.file_writer import (
+    write_to_json_file,
+    write_to_csv_file,
+    write_to_txt_file,
+)
 
 
-nl_nl = "\n\n"
-
-
-def format_as_json(data: Union[Dict[str, Any], List[Any]]) -> str:
-    return json.dumps(data, indent=4)
-
-
-# The parameter 'data' can be either a Dict, or a List
-def format_as_csv(data: Union[Dict[str, Any], List[Any]]) -> str:
-    output = io.StringIO()
-    if isinstance(data, Dict):
-        writer = csv.DictWriter(output, fieldnames=data.keys())
-        writer.writeheader()
-        writer.writerow(data)
+def output_format(data: Dict[str, Any], format_type: str, target_output: str):
+    if target_output == "terminal":
+        if format_type == "json":
+            return format_as_json(data)
+        elif format_type == "csv":
+            return format_as_csv(data)
+        elif format_type == "txt":
+            return format_as_txt(data)
+        else:
+            raise TypeError("Supported formats are: (json, csv, txt)")
     else:
-        fieldnames = [
-            "date",
-            "temp",
-            "sensation",
-            "description",
-            "humidity",
-            "pressure",
-            "wind_speed",
-            "visibility",
-        ]
-        writer = csv.DictWriter(output, fieldnames=fieldnames)
-        writer.writeheader()
-        for d in data:
-            writer.writerow(d)
-    return output.getvalue()
-
-
-def format_as_txt(data: Union[Dict[str, Any], List[Any]]) -> str:
-    if isinstance(data, Dict):
-        return "\n".join(f"{key.capitalize()}: {value}" for key, value in data.items())
-    else:
-        return nl_nl.join(
-            "\n".join(f"{key.capitalize()}: {value}" for key, value in item.items())
-            for item in data
-        )
-
-
-def output_format(data: Dict[str, Any], format_type: str):
-    if format_type == "json":
-        return format_as_json(data)
-    elif format_type == "csv":
-        return format_as_csv(data)
-    elif format_type == "txt":
-        return format_as_txt(data)
-    else:
-        raise TypeError("Supported formats are: (json, csv, txt)")
+        if format_type == "json":
+            write_to_json_file()
+        elif format_type == "csv":
+            write_to_csv_file()
+        elif format_type == "txt":
+            write_to_txt_file()
