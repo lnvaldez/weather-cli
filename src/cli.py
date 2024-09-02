b8847c51310a0be56b426bd7cd86466795aac941
Lucas Valdez <lnvaldez24@gmail.com>
2024-09-02 12:54:38 -0400

    Register 'clear' function as a Typer command

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,13 @@
 import typer
 from src.commands.weather_commands import location, coordinate
+from src.commands.util_commands import clear
 
 app = typer.Typer()
 
 
 app.command()(location)
 app.command()(coordinate)
+app.command()(clear)
 
 
 if __name__ == "__main__":
