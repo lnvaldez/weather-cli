c16fda918ae6388a6dc2e3beb7b0e4031ee214b7
Lucas Valdez <lnvaldez24@gmail.com>
2024-09-02 11:21:51 -0400

    Remove unused imports

--- src/cli.py
+++ src/cli.py
@@ -1,52 +1,11 @@
 import typer
-from src.models.weather_models import WeatherLocation, Coordinate
-from src.utils.enum import OutputFormat
-from src.utils.output_format import output_format
+from src.commands.weather_commands import location, coordinate
 
 app = typer.Typer()
 
 
-@app.command()
-def location(
-    city: str,
-    current: bool = False,
-    forecast: bool = False,
-    aqi: bool = False,
-    sun: bool = False,
-    format: OutputFormat = OutputFormat.json,
-):
-    weather = WeatherLocation(city_name=city)
-
-    if current:
-        print(output_format(data=weather.current_data, format_type=format))
-    if forecast:
-        print(output_format(data=weather.forecast, format_type=format))
-    if aqi:
-        print(output_format(data=weather.air_quality, format_type=format))
-    if sun:
-        print(output_format(data=weather.sun_data, format_type=format))
-
-
-@app.command()
-def coordinate(
-    latitude: float,
-    longitude: float,
-    current: bool = False,
-    forecast: bool = False,
-    aqi: bool = False,
-    sun: bool = False,
-    format: OutputFormat = OutputFormat.json,
-):
-    weather = Coordinate(lat=latitude, lon=longitude)
-
-    if current:
-        print(output_format(data=weather.current_data, format_type=format))
-    if forecast:
-        print(output_format(data=weather.forecast, format_type=format))
-    if aqi:
-        print(output_format(data=weather.air_quality, format_type=format))
-    if sun:
-        print(output_format(data=weather.sun_data, format_type=format))
+app.command()(location)
+app.command()(coordinate)
 
 
 if __name__ == "__main__":
